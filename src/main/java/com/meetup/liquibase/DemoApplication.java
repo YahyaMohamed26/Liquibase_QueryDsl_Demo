63bc0f78efa500c726931d0b61c7c9596c5d4b49
ertugrulaktas <ertugrul@layermark.com>
2022-09-19 15:46:28 +0300

    QueryDsl old version

--- src/main/java/com/meetup/liquibase/DemoApplication.java
+++ src/main/java/com/meetup/liquibase/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.meetup.liquibase;
 
-package com.meetup.liquibase;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
